28cc2e31e0eb5ef12fb4e575be0434268c71c978
Alston253253 <madibualstontadiwanashe.144217@marwadiuniversity.ac.in>
2026-08-18 15:03:44 +0530

    Add files via upload

---
+++ PROG26UNIT3.java
@@ -0,0 +1,17 @@
+public class Main {
+    static void checkAge(int age) {
+        if (age < 18) {
+            throw new ArithmeticException("Access denied - You must be at least 18 years old.");
+        } else {
+            System.out.println("Access granted - You are old enough.");
+        }
+    }
+
+    public static void main(String[] args) {
+        try {
+            checkAge(15);
+        } catch (ArithmeticException e) {
+            System.out.println("Caught Exception: " + e.getMessage());
+        }
+    }
+}
