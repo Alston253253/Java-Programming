d33b2a17ceee152486a7250b7c418910acd32c94
Alston253253 <madibualstontadiwanashe.144217@marwadiuniversity.ac.in>
2026-08-18 14:27:00 +0530

    Add files via upload

---
+++ program6.java
@@ -0,0 +1,16 @@
+class payral
+{
+    public static void main(String args[])
+	{
+	    int i,i;
+		for(i=1;i<=5;i++)
+		{
+		    for(j=1,j<=i;j++);
+			{
+			    system.out.print(j+"");
+				
+				}
+			system.out.printIn("");
+            }
+        }
+    }		
