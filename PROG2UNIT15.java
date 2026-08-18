5de7fb9d58462af88b0aa958d66fdb59d3dcef0a
Alston253253 <madibualstontadiwanashe.144217@marwadiuniversity.ac.in>
2026-08-18 14:49:06 +0530

    Add files via upload

---
+++ PROG2UNIT15.java
@@ -0,0 +1,32 @@
+class Animal {
+    void eat() {
+        System.out.println("Eating...");
+    }
+}
+
+class Dog extends Animal {
+    void bark() {
+        System.out.println("Barking...");
+    }
+}
+
+class Cat extends Animal {
+    void meow() {
+        System.out.println("Meowing...");
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Dog dog = new Dog();
+        Cat cat = new Cat();
+
+        dog.eat();
+        dog.bark();
+
+        System.out.println();
+
+        cat.eat();
+        cat.meow();
+    }
+}
